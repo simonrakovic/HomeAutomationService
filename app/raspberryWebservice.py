d4f1ed44d40a992d6289947c14b396346fe757bc
simon <you@example.com>
2018-05-25 14:58:34 +0200

    fixes

--- app/raspberryWebservice.py
+++ app/raspberryWebservice.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+from flask import request
 from celery import Celery
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
+import json
 import time
 
 app = Flask(__name__)
@@ -31,12 +35,33 @@ def api_zalivaj(id, cas):
 def api_ustavi_zalivanje():
     return "done"
 
+@app.route('/api/get/timer', methods=['GET'])
+def api_vsi_timerji():
+
+    return json.dumps(db.all())
+
+@app.route('/api/delete/timer/<int:index>', methods=['GET'])
+def api_izbrisi_timer(index):
+    db.remove(Query().field == index+1)
+    return "deleted"
+
+@app.route('/api/add/timer', methods=['POST'])
+def api_dodaj_timer():
+    schedule = request.json
+    index = db.insert(schedule)
+    return str(index-1)
+
+@app.route('/timer')
+def timer():
+    return render_template('timeschedule.html' )
+
 @app.route('/')
 def index():
     teamplateData = {
         "title": "hello"
     }
     return render_template('index.html', **teamplateData)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
